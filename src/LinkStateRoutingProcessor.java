18c978f3646c68a313a782949b0f80f7faa7b8e5
azaiter <azaiter@svsu.edu>
2017-11-19 20:10:07 -0500

    Add documentation (javadocs) on LinkStateRoutingProcessor

--- src/LinkStateRoutingProcessor.java
+++ src/LinkStateRoutingProcessor.java
@@ -9,35 +9,75 @@ import java.util.Scanner;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
- * Created by Zaiter on 11/18/2017.
+ * <h1>LinkStateRoutingProcessor</h1>
+ *
+ * <h2>LS Routing main application runner. It acts as a simulator for a single LS link in a network.</h2>
+ *
+ * <p>It does the Following:</p>
+ * <ol>
+ *     <li>Runs the server that will be acting as the communicator about current LS link with other links that request
+ *     the graph topology.</li>
+ *     <li>Acts as a client that requests the network topology from all of its intermediate connections</li>
+ *     <li>Prints the shortest path from its LS link to all other connected LS links in the network
+ *     that have been known</li>
+ * </ol>
+ *
+ * Created by Zack Zaiter (Abdulrahman) & Ryan Castiglione on 11/18/2017.
+ *
+ * @author Zack Zaiter (Abdulrahman) & Ryan Castiglione.
+ * @since 11/18/2017
  */
 public class LinkStateRoutingProcessor {
-    public static final String SERVER_IP = "127.0.0.1";
+    // a Constant that holds the server ip of the communicator.
+    private static final String SERVER_IP = "127.0.0.1";
+
+    /**
+     * a main function that runs the interactive window function
+     *
+     * @param args program caller arguments
+     * @throws Exception when the LSPacket fails to initiate from a settings file
+     */
     public static void main(String[] args) throws Exception {
+        showInteractiveMenu(args);
+    }
+
+    /**
+     * A method that shows the main interactive window of the program.
+     *
+     * @param args program caller arguments
+     * @throws Exception when the LSPacket fails to initiate from a settings file
+     */
+    public static void showInteractiveMenu(String[] args) throws Exception {
+        // initialize the variables
         Scanner sc = new Scanner(System.in);
         int choice;
         LSPacket lsPacket = new LSPacket(args[0]);
         boolean serverRunning = false;
         boolean clientRunning = false;
 
+        // do a loop that will ask the user of their choice.
         do {
             printMenu(serverRunning, clientRunning);
             try {
                 choice = sc.nextInt();
                 switch (choice) {
+                    // 1- run the server
                     case 1:
                         if (!serverRunning) {
                             serverRunning = true;
                             runServer(lsPacket);
                         } else System.out.println("Server is running! you can't run it twice.");
                         break;
+                    // 2- run the clients communicator
                     case 2:
                         clientRunning = true;
                         runClient(lsPacket);
                         break;
+                    // 3- print current LS shortest path from this node to all others
                     case 3:
                         System.out.println(lsPacket);
                         break;
+                    // 4- exit the program
                     case 4:
                         System.exit(0);
                         break;
@@ -52,29 +92,39 @@ public class LinkStateRoutingProcessor {
         } while(true);
     }
 
+    /**
+     * A method to run the server that will be acting as the communicator about current LS link with other links that
+     * request the graph topology.
+     *
+     * @param lsPacket a valid LSPacket instance.
+     */
     public static void runServer(LSPacket lsPacket){
+        // run the server as a thread that spawns threads for each LS packet connection ( Concurrency Control )
         new Thread(()->{
             try {
+                //initiate server socket
                 ServerSocket srvSocket = new ServerSocket(lsPacket.serverPort);
                 try {
                     while (true) {
+                        // accept client connection and serve it in a new thread.
                         Socket socket = srvSocket.accept();
                         // initialize input and output streams.
                         new Thread(() -> {
                             try {
+                                // initiate a reentrant lock when accessing the LSPacket instance and updating the topography.
                                 ReentrantLock lock = new ReentrantLock();
                                 ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
-                                ObjectOutput outputStream = new ObjectOutputStream(socket.getOutputStream());
-                                //while (true)
-                                {
-                                    Object objectToRead = inputStream.readObject();
-                                    socket.close();
-                                    lock.lock();
-                                    SimpleDirectedWeightedGraph<String, DefaultWeightedEdge> clientLSPacketGraph =
-                                            (SimpleDirectedWeightedGraph<String, DefaultWeightedEdge>) objectToRead;
-                                    lock.unlock();
-                                    lsPacket.addEdgesFromAnotherLSPacket(clientLSPacketGraph);
-                                }
+                                Object objectToRead = inputStream.readObject();
+
+                                //close the socket as soon as it gets the needed graph topology.
+                                socket.close();
+
+                                // lock the thread, update the local topology and unlock the reentrant lock
+                                lock.lock();
+                                SimpleDirectedWeightedGraph<String, DefaultWeightedEdge> clientLSPacketGraph =
+                                        (SimpleDirectedWeightedGraph<String, DefaultWeightedEdge>) objectToRead;
+                                lsPacket.addEdgesFromAnotherLSPacket(clientLSPacketGraph);
+                                lock.unlock();
                             } catch (Exception e) {
                                 e.printStackTrace();
                             }
@@ -87,18 +137,22 @@ public class LinkStateRoutingProcessor {
         }).start();
     }
 
-
+    /**
+     * A client runner that requests the network topology from all of its intermediate connections
+     *
+     * @param lsPacket
+     */
     public static void runClient(LSPacket lsPacket){
+        // run a thread for each client request to all connected adjacent LS links
         new Thread(()->{
-            while(true)
-            {
+            while(true){
                 for (Integer portNum : lsPacket.adjacentLSPacketsPorts) {
                     new Thread(() -> {
                         try {
+                            // initiate a lock, server the LS topography and close the connections.
                             ReentrantLock lock = new ReentrantLock();
                             Socket socket = new Socket(SERVER_IP, portNum);
                             ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
-                            ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
                             lock.lock();
                             outputStream.writeObject(lsPacket.lsPacketGraph);
                             lock.unlock();
@@ -108,14 +162,19 @@ public class LinkStateRoutingProcessor {
                         }
                     }).start();
                     try {
-                        Thread.sleep(5000);
+                        Thread.sleep(5000); // sleep for 5 seconds (pulse sleep) to lessen the load on the network
                     }catch (Exception e) {e.printStackTrace();}
                 }
             }
         }).start();
     }
 
-
+    /**
+     * a method to print the main menu.
+     *
+     * @param serverRunning a boolean that indicates if the server is running or not
+     * @param clientRunning a boolean that indicates if the client communicator is running or not
+     */
     public static void printMenu(boolean serverRunning, boolean clientRunning){
         System.out.println(
                         "1 - Run LSPacket Server. Currently: "+serverRunning+"\n" +
